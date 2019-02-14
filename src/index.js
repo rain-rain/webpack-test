67449db4b204d9d5bbb0c8e85b302c2967ffa6ed
zhaohaiying <zhaohaiying@sensorsdata.cn>
2019-02-14 21:06:32 +0800

    打包 html

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import foo from './foo';
 console.log(foo);
 
 console.log('hi webpack~');
+
+document.write('<h2 class="border">hi webpack~</h2>')
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  // mode: 'production',
+  // mode: 'development',
+  mode: 'production',
   // 这里是打包的入口文件(相对路径)
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     // 打包结果存放的位置(必须用绝对路劲)
     path: path.resolve(__dirname, 'dist'),
     // path: path.resolve('/Users/rainzhao/collect/webpack/demo/1', 'dist'),
     // 打包结果文件名称
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
   },
+  module: {
+    rules: [{ test: /\.css$/, use: 'css-loader' }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // 模板文件          
+      filename: 'index.html'
+    })
+  ]
+
 };
