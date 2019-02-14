ec65f8d9e714abaf18c323f661c1be3b307a0b91
zhaohaiying <zhaohaiying@sensorsdata.cn>
2019-02-14 23:14:44 +0800

    单独打包 css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Webpack = require('webpack');
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Webpack = require('webpack')
 
 module.exports = {
-  // mode: 'development',
-  mode: 'production',
+  mode: 'development',
+  // mode: 'production',
   // 这里是打包的入口文件(相对路径)
   entry: './src/index.js',
   output: {
@@ -12,17 +13,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // path: path.resolve('/Users/rainzhao/collect/webpack/demo/1', 'dist'),
     // 打包结果文件名称
-    filename: 'bundle.[hash].js',
+    filename: 'index.[hash:8].js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash:8].css",
+      chunkFilename: "[id].css"
+    }),
     new Webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html', // 模板文件          
@@ -30,4 +35,4 @@ module.exports = {
     })
   ]
 
-};
+}
