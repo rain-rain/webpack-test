19562c172cfcb30fb3cae880a9e0fc557dc969d5
zhaohaiying <zhaohaiying@sensorsdata.cn>
2019-02-16 15:38:06 +0800

    ...

--- dist/simple.js
+++ dist/simple.js
@@ -1,26 +1,20 @@
-// 混合
+
+
+
+// commonjs 模块化的处理
 (function(modules) {})({
   "./foo.js": (function (module, exports) {
     module.exports = {
       name: 'zhy',
       value: 'hello webpack',
     };
-    export default {
-      name: 'zhy',
-      value: 'hello webpack',
-    };
   }),
-  "./index.js": (function (module, __webpack_exports__, __webpack_require__) {
-    "use strict";
-    __webpack_require__.r(__webpack_exports__);
-    var _foo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./foo.js");
-    var _foo__WEBPACK_IMPORTED_MODULE_0___default = __webpack_require__.n(_foo__WEBPACK_IMPORTED_MODULE_0__);
-    const foo = require('./foo');
-    console.log(_foo__WEBPACK_IMPORTED_MODULE_0___default.a);
+  "./index.js": (function (module, exports, __webpack_require__) {
+    const foo = __webpack_require__("./foo.js");
+    console.log(foo);
     console.log('hi webpack~');
   })
 });
-
 
 // es6 Module 模块化的处理
 (function(modules) {})({
@@ -46,18 +40,25 @@
   })
 });
 
-
-// commonjs 模块化的处理
+// 混合
 (function(modules) {})({
   "./foo.js": (function (module, exports) {
     module.exports = {
       name: 'zhy',
       value: 'hello webpack',
     };
+    export default {
+      name: 'zhy',
+      value: 'hello webpack',
+    };
   }),
-  "./index.js": (function (module, exports, __webpack_require__) {
-    const foo = __webpack_require__("./foo.js");
-    console.log(foo);
+  "./index.js": (function (module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+    __webpack_require__.r(__webpack_exports__);
+    var _foo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./foo.js");
+    var _foo__WEBPACK_IMPORTED_MODULE_0___default = __webpack_require__.n(_foo__WEBPACK_IMPORTED_MODULE_0__);
+    const foo = require('./foo');
+    console.log(_foo__WEBPACK_IMPORTED_MODULE_0___default.a);
     console.log('hi webpack~');
   })
 });
